208bb3e1b58634fe1ad9c4f6a0bd4b5b698e2f5a
Fabrizio Mirabito <f.mirabito@arduino.cc>
2020-01-15 15:07:07 +0100

    Change token request url

--- example/main.js
+++ example/main.js
@@ -4,7 +4,7 @@ var rp = require('request-promise');
 async function getToken() {
     var options = {
         method: 'POST',
-        url: 'https://login.arduino.cc/oauth/token',
+        url: 'https://api2.arduino.cc/iot/v1/clients/token',
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         json: true,
         form: {
